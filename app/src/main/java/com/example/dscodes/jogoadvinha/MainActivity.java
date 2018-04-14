d118c357df1763e82995a0c4a5bb0d24231bf9e1
DarlanSimplicio <darlansimplicio@gmail.com>
2018-04-14 02:09:15 -0300

    SegundoCommit

--- app/src/main/java/com/example/dscodes/jogoadvinha/MainActivity.java
+++ app/src/main/java/com/example/dscodes/jogoadvinha/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.dscodes.jogoadvinha;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn ;
+    private Button btnProxi ;
     private TextView result ;
 
     @Override
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = (Button) findViewById(R.id.botao) ;
+        btnProxi = (Button) findViewById(R.id.btnProxima);
         result = (TextView) findViewById(R.id.resultado) ;
 
 
@@ -27,6 +31,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 result.setText("Nº sorteado: " + randomica());
+                Toast.makeText(getApplicationContext(), "O jogo foi inciado..", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnProxi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Nova Aplicação Aberta!", Toast.LENGTH_LONG ).show();
+                startActivity(new Intent(MainActivity.this, SegundaActivity.class)) ;
             }
         });
     }
